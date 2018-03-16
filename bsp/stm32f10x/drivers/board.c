3f05dd0427542424937637c0bf3dfb33f6740539
VectorShen <sqj@anchorelec.com>
2018-03-16 23:54:10 +0800

    board.c实现了rt_hw_us_delay

--- bsp/stm32f10x/drivers/board.c
+++ bsp/stm32f10x/drivers/board.c
@@ -166,6 +166,18 @@ void SysTick_Handler(void)
     rt_interrupt_leave();
 }
 
+void rt_hw_us_delay(rt_uint32_t us)
+{
+  	rt_uint32_t delta;
+	/* 获得延时经过的tick数 */
+	us = us * (SysTick->LOAD/(1000000/RT_TICK_PER_SECOND));
+	/* 获得当前时间 */
+	delta = SysTick->VAL;
+	/* 循环获得当前时间，直到达到指定的时间后退出循环 */
+	while (delta - SysTick->VAL< us)
+		;
+}
+
 /**
  * This function will initial STM32 board.
  */
