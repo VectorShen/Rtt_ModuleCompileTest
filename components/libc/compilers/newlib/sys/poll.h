7b0050e4db39f37517e6b3d0cf19ffb495ce0aff
VectorShen <sqj@anchorelec.com>
2018-03-16 13:31:44 +0800

    newlib的系统头文件优化

---
+++ components/libc/compilers/newlib/sys/poll.h
@@ -0,0 +1,6 @@
+#ifndef _NEWLIB_SYS_POLL_H_
+#define _NEWLIB_SYS_POLL_H_
+
+#include "ipc/poll.h"
+
+#endif /* _NEWLIB_SYS_POLL_H_ */
---
+++ components/libc/compilers/newlib/sys/signal.h
@@ -0,0 +1,6 @@
+#ifndef _NEWLIB_SYS_SIGNAL_H_
+#define _NEWLIB_SYS_SIGNAL_H_
+
+#include "libc/libc_signal.h" 
+
+#endif /* _NEWLIB_SYS_SIGNAL_H_ */
